1f55ae5f51e8172a840026e5a5522273a64ca921
Viktor Aziatskii <viktoraziatskyi@gmail.com>
2023-07-06 23:16:49 +0300

    исправляет

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
-const idPhoto = 25;
+const ID_PHOTOS = 25;
 
-const authorName = [
-    'Федя',
-    'Петя',
-    'Аня',
-    'Ваня',
-    'Василиса',
-    'Настя',
-    'Оля',
-    'Коля',
-    'Веня',
-    'Илона'
+const AUTHOR_NAMES = [
+  'Федя',
+  'Петя',
+  'Аня',
+  'Ваня',
+  'Василиса',
+  'Настя',
+  'Оля',
+  'Коля',
+  'Веня',
+  'Илона'
 ];
 
-const messagePhoto = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+const MESSAGE_PHOTOS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-const descriptionPhoto = [
+const DESCRIPTION_PHOTOS = [
   'Кекс на прогулке',
   'поездка на машине',
   'море, горы, солнце',
@@ -35,6 +35,18 @@ const descriptionPhoto = [
   'погружение с аквалалангом'
 ];
 
+// eslint-disable-next-line no-unused-vars
+const MAX_COMMENTS = 5;
+
+// eslint-disable-next-line no-unused-vars
+const MIN_LIKES = 15;
+
+// eslint-disable-next-line no-unused-vars
+const MAX_LIKES = 200;
+
+// eslint-disable-next-line no-unused-vars
+const AVATAR_COUNT = 6;
+
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -45,16 +57,16 @@ const getRandomInteger = (a, b) => {
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 const createComment = () => ({
-  id: getRandomInteger(1, 25),
+  id: getRandomInteger(1, 10),
   avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-  message: getRandomArrayElement(messagePhoto),
-  name: getRandomArrayElement(authorName)
+  message: getRandomArrayElement(MESSAGE_PHOTOS),
+  name: getRandomArrayElement(AUTHOR_NAMES)
 });
 
 const createDescription = (_, index) => ({
   id: index + 1,
   url: `photos/${index + 1}.jpg`,
-  description: getRandomArrayElement(descriptionPhoto),
+  description: getRandomArrayElement(DESCRIPTION_PHOTOS),
   likes: getRandomInteger(15, 200),
   comments: Array.from({
     length: getRandomInteger(0, 5) },
@@ -62,6 +74,7 @@ const createDescription = (_, index) => ({
   )
 });
 
-const similarDescription = Array.from({length: idPhoto}, createDescription);
+const similarDescription = Array.from({length: ID_PHOTOS}, createDescription);
 
+// eslint-disable-next-line no-console
 console.log(similarDescription);
