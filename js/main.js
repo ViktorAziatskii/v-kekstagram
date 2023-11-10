3f4db9602978660ae2b0baf1d10c351add6ff436
Viktor Aziatskii <viktoraziatskyi@gmail.com>
2023-11-10 23:38:34 +0300

    надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,14 +1,36 @@
 import './data.js';
 
-import './util.js';
+import { showAlert } from './util.js';
 
 import './user-form.js';
 
 import { renderGallery } from './gallery.js';
 
-import { similarDescription } from './data.js';
+import { getData, sendData } from './api.js';
+
+import { showSuccessMessage, showErrorMessage } from './message.js';
+
+import { setOnFormSubmit, hideModal } from './user-form.js';
 
 import { renderThumbnails } from './thumbnail.js';
 
+import { similarDescription } from './data.js';
+
 renderThumbnails(similarDescription);
-renderGallery(similarDescription);
+
+setOnFormSubmit(async (data) => {
+  try{
+    await sendData(data);
+    hideModal();
+    showSuccessMessage();
+  } catch {
+    showErrorMessage();
+  }
+});
+
+try {
+  const data = await getData();
+  renderGallery(data);
+} catch (err) {
+  showAlert(err.message);
+}
